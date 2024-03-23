aec4d92a69c6958cd66c13e49e5bff30a4e9217a
Charles Thomas <ch@rlesthom.as>
2024-03-23 17:15:21 -0400

    feat: address the fact that the dashboard configmap is too large
    by making 26 configmaps, split by first letter of the dashboard.json's
    filename

--- generate-dashboards.sh
+++ generate-dashboards.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
+set -e
 
-# this script loads in all dashboard files in grafana/dashboards/*.json
-# into a single configmap, which then gets mounted as /etc/dashboards.
+# this script loads in all dashboard files in dashboards/*.json
+# into a set of configmaps by the first letter of the filename,
+# each of which then gets mounted as /etc/dashboards/$prefix.
 # values.yaml includes a dashboardProvider to load all the dashboards from /etc/dashboards
 
-cp etc-dashboards.partial.yaml .etc-dashboards.in.yaml
-for yaml in $(ls dashboards/*.json); do
-    bname=$(basename $yaml)
-    cmd="yq '.data.\"${bname}\" = load_str(\"${yaml}\")' .etc-dashboards.in.yaml > .etc-dashboards.out.yaml"
-    eval $cmd
-    mv .etc-dashboards.out.yaml .etc-dashboards.in.yaml
+[ -e resources/configmaps.yaml ] && rm resources/configmaps.yaml
+
+for prefix in {a..z}; do
+    eval "yq '.metadata.name = \"etc-dashboards-${prefix}\"' etc-dashboards.partial.yaml > \"etc-dashboards-${prefix}.yaml\""
+    # have to use eval b/c of all the quoting yq requires and the singles are screwing with bash variables
+    for yaml in $(find dashboards -name ${prefix}*.json); do
+        exists=1
+        bname=$(basename $yaml)
+        eval "yq -i '.data.\"${bname}\" = load_str(\"${yaml}\")' \"etc-dashboards-${prefix}.yaml\""
+    done
+    cat etc-dashboards-${prefix}.yaml >> resources/configmaps.yaml
+    rm etc-dashboards-${prefix}.yaml
 done
-mv .etc-dashboards.in.yaml resources/configmaps.yaml
